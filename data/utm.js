bbe532214648077ad1a5d0f83debabcef45977c8
Vincent Link <vincent-link@t-online.de>
2014-05-11 17:55:47 +0200

    Remove only affected parameters

--- data/utm.js
+++ data/utm.js
@@ -1,3 +1,6 @@
 if (/[?&]utm_/.test(location.search) && history.replaceState){
-    history.replaceState(null,'', location.href.replace(/[?&]utm_.*/,''));
+    // Replace the first of n>1 parameters
+    var address = location.href.replace(/\?utm_[^&]*&/,'?');
+    // Replace other parameters
+    history.replaceState(null,'', address.replace(/[?&]utm_[^&]*/g,''));
 }
