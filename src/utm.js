602aed45ccd6473954c95e726d3617493ef166ae
Anthony Ricaud <anthony@ricaud.me>
2018-02-13 15:46:02 +0000

    Removes utm_ found in hash part of the URL
    - Also fixes an issue where only one out of two parameters would be deleted
    
    fix #4

--- src/utm.js
+++ src/utm.js
@@ -11,11 +11,20 @@ class UTM {
 
     static remove(url) {
         const parsedURL = new URL(url)
-        for (let param of parsedURL.searchParams.keys()) {
+        for (let param of [...parsedURL.searchParams.keys()]) {
             if (param.startsWith(utmPrefix)) {
                 parsedURL.searchParams.delete(param)
             }
         }
+
+        const parsedFragment = new URLSearchParams(parsedURL.hash.substring(1))
+        for (let param of [...parsedFragment.keys()]) {
+            if (param.startsWith(utmPrefix)) {
+                parsedFragment.delete(param)
+            }
+        }
+        parsedURL.hash = parsedFragment.toString()
+
         return parsedURL.toString()
     }
 }
--- test/utm.js
+++ test/utm.js
@@ -17,6 +17,18 @@ describe('UTM.remove', function() {
             input: 'http://foo.com/?bar=baz&utm_source=12&foo=true',
             output: 'http://foo.com/?bar=baz&foo=true',
         },
+        {
+            title: 'removes all utm_ parameters',
+            input: 'http://foo.com?utm_one=one&utm_two=two',
+            output: 'http://foo.com/',
+        },
+        {
+            title: 'removes utm_ parameters in hash',
+            input:
+                'http://www.fredzone.org/la-terre-a-t-elle-ete-ensemencee-par-des-particules-extraterrestres-554#utm_source=feed&utm_medium=feed&utm_campaign=feed',
+            output:
+                'http://www.fredzone.org/la-terre-a-t-elle-ete-ensemencee-par-des-particules-extraterrestres-554',
+        },
     ].forEach(function({ title, input, output }) {
         it(title, function() {
             expect(UTM.remove(input)).to.equal(output)
